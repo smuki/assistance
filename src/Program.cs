d85ae31c4b0f70047653a694ded7e3332e91bdd0
sunriseyuen <tiptoes@foxmail.com>
2025-11-06 14:36:57 +0800

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -2,16 +2,28 @@ namespace WinFormsApp1
 {
     internal static class Program
     {
+        private static Mutex mutex = new Mutex(true, "OnlyRun");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new frmTools());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                ApplicationConfiguration.Initialize();
+                Application.Run(new frmTools());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                Application.Exit();
+            }
         }
     }
 }
